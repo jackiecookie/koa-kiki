296b57c40a5e7fa8e7a3da452d0c882231bce3a6
jackiecookie <2711002357@qq.com>
2016-09-23 01:54:14 -0700

    c

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
+import Koa from 'koa';
+import hbs from 'koa-handlebars-pdh';
 
-var Koa = require('koa');
+const router =  require('koa-router')();;
+
+
 const app = new Koa();
 
-app.use(async function (ctx, next) {
-    console.log('>> one');
-    await next();
-    console.log('<< one');
+app
+    .use(hbs())
+    .use(router.routes())
+    .use(router.allowedMethods());
+
+
+
+router.get('/test', function *(next) {
+    this.body='first test';
+    yield next;
 });
 
-app.use(async function (ctx, next) {
-    console.log('>> two');
-    ctx.body = 'two';
-    await next();
-    console.log('<< two');
-});
-
-app.use(async function (ctx, next) {
-    console.log('>> three');
-    await next();
-    console.log('<< three');
-});
-
-app.listen(3000);
+app.listen(30010);
