e2f01f54695e062aeeb1ae0688e7a28749258e50
jackiecookie <2711002357@qq.com>
2016-10-10 09:44:03 +0800

    commit

--- app/controller/Home.js
+++ app/controller/Home.js
@@ -3,21 +3,18 @@ import {method, action, parameters} from 'koa-kiki-router/decorators/decorator'
 class Index {
     @action('/')
     async root(ctx, next) {
-        ctx.body = 'success';
-        await next;
-    }
-
-    @method('post')
-    async home(ctx, next) {
-        ctx.body = 'success';
-        await next;
+        ctx.body = await ctx.render('Home/root.hbs',{
+            title:'home/root',
+            body:'test'
+        });
+        await next();
     }
 
     @method('get')
     @parameters(':id')
     async parameter(ctx, next) {
-        ctx.body =ctx.params.id;
-        await next;
+        ctx.body = ctx.params.id;
+        await next();
     }
 
 }
